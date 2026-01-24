1ff2e42f5c240874fad1bbd80de016f58faccddc
Pavani Edirisinghe <eg245148@engug.ruh.ac.lk>
2026-01-24 23:09:19 +0530

    Changed instance type to t3.micro

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ data "aws_ami" "ubuntu" {
 # 3. Create EC2 Instance
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "calmspace-new-key" 
   security_groups = [aws_security_group.calmspace_sg.name]
 
